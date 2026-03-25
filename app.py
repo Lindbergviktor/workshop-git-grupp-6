617eae728ec4cda54d645b520b2badecde52eb1e
Lindbergviktor <viktorlindbergstudent@gmail.com>
2026-03-25 15:51:52 +0100

    Feat: aa__main__guard

--- app.py
+++ app.py
@@ -13,3 +13,5 @@ def run():
         print(mul(a, b))
     if choice == "4" and b != 0: print("Error: division by zero")
     if choice == "4" and b != 0: print(div(a, b))
+    if __name__ == "__main__":
+        main ()
