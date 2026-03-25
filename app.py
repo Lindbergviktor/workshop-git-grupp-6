7717690d33d50acce36df05538113c3db17e2ff1
wictornyman <wictor.nyman@gmail.com>
2026-03-25 15:02:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ from calculator import add
 def run():
     print("1) Subtact\n0) Exit")
     choice = input("Select: ")
-
-if choice == "1": print("TODO Add")
+    if choice == "1": print("TODO Add")
